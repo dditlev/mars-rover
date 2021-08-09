6734fe2250a0e9708168447f2aef932988e3171e
Ditlev Tøjner <tojner@gmail.com>
2021-08-10 01:05:10 +0200

    added comments to rover-source

--- server/rover-source.js
+++ server/rover-source.js
@@ -1,7 +1,17 @@
 const https = require("https");
 const MultiStream = require("multistream");
 const stream = require("stream");
+
+/**
+ * Marks the seperation of streams
+ */
 const BOUNDARY_MARKER = "\n";
+
+/**
+ * Consume the API with a path (or get the metadata with no path)
+ * @param {string} path the pathname of the api endpoint
+ * @returns http.IncomingMessage (stream.Readable)
+ */
 function consumer(path) {
   return new Promise((resolve, reject) => {
     https.get(
@@ -12,11 +22,21 @@ function consumer(path) {
     );
   });
 }
+/**
+ * Yield each index on next()
+ * @param {array} range of indicies
+ */
 function* yieldTheRange(range) {
   for (const index of range) {
     yield index;
   }
 }
+/**
+ * Create a range of n numbers with an optional offset (startAt)
+ * @param {number} size the size of the range
+ * @param {number} startAt the offset of the range
+ * @returns Array
+ */
 function makeRange(size, startAt = 0) {
   return [...Array(size).keys()].map((i) => i + startAt);
 }
@@ -26,6 +46,8 @@ module.exports = {
     const take = parseInt(q.take);
     const from = parseInt(q.from);
 
+    // Create the range for the generator and append the boundary marker
+    // after each index in the range
     const streams = yieldTheRange(
       makeRange(take, from).reduce((newLined, index) => {
         newLined.push(index);
@@ -37,9 +59,13 @@ module.exports = {
     return new MultiStream((cb) => {
       const { value, done } = streams.next();
       if (done) return cb(null, null);
+      // if the value in the range is the BOUNDARY_MARKER create a readstream
+      // with the marker and return it to MultiStream, to indicate the end of
+      // one stream and the beginning of another stream
       if (value === BOUNDARY_MARKER) {
         return cb(null, stream.Readable.from([BOUNDARY_MARKER]));
       }
+      // If the value is not a marker, fetch the stream and pass it to MultiStream
       consumer(value).then((stream) => {
         cb(null, stream);
       });
