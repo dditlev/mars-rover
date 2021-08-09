280a119aa02f2249698999736acfb8d04455b7f7
Ditlev Tøjner <tojner@gmail.com>
2021-08-10 00:26:45 +0200

    added description to jest React injector

--- client/testapp.js
+++ client/testapp.js
@@ -1,3 +1,8 @@
+/**
+ * There is no bundling, the React dependency is loaded through unpkg
+ * via the html file. To allow Jest to test, we bind React to 
+ * window, like when loading through unpkg.
+ */
 import React from "react"
 import {Home as H, Single as S} from "./app"
 export function Home(props) {
