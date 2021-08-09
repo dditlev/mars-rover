e240897f6a588485a2aa54a3c345e61e1faaf0df
Ditlev Tøjner <tojner@gmail.com>
2021-08-10 01:12:47 +0200

    uncommented tests

--- server/run.test.js
+++ server/run.test.js
@@ -2,30 +2,25 @@ const request = require("supertest");
 const server = require("./run");
 
 describe("Test the express server", () => {
-//   it("Get the homepage", async () => {
-//     return request(server)
-//       .get("/")
-//       .then(response => {
-//         expect(response.statusCode).toBe(200);
-//       });
-//   });
-//   it("Get an index", async () => {
-//     return request(server)
-//       .get("/222")
-//       .then(response => {
-//         expect(response.statusCode).toBe(200);
-//       });
-//   });
-//   it("Get redirected to home if index is not a number", async () => {
-//     return request(server)
-//       .get("/thisisnotanumber")
-//       .expect(302)
-//   });
-//   it("Get redirected to home if index does not exists", async () => {
-//     return request(server)
-//       .get("/999999999999")
-//       .expect(302)
-//   });
+  it("Get the homepage", async () => {
+    return request(server)
+      .get("/")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+      });
+  });
+  it("Get an index", async () => {
+    return request(server)
+      .get("/222")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+      });
+  });
+  it("Get redirected to home if index is not a number", async () => {
+    return request(server)
+      .get("/thisisnotanumber")
+      .expect(302)
+  });
   test("Starts autoplay", async () => {
     return new Promise((resolve) => {
         request(server)
